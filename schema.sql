b0575eccd92c8c16236a2d0d5d0c1e660985c0b3
Olivier Mansuy <oliviermansuy7@gmail.com>
2024-12-15 12:35:43 -0500

    schema update

--- schema.sql
+++ schema.sql
@@ -15,7 +15,26 @@ CREATE TABLE IF NOT EXISTS user(
     username VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
     pwd VARCHAR(100) NOT NULL,
+    added_date DATETIME DEFAULT (CURRENT_DATE()),
     PRIMARY KEY (user_id)
 );
 
-insert into user(username, email, pwd) values("slt", "slt", "slt");
+DROP TABLE IF EXISTS rel_friend;
+CREATE TABLE IF NOT EXISTS rel_friend(
+    rel_id INT AUTO_INCREMENT, 
+    id_origin_user INT NOT NULL,
+    id_added_user INT NOT NULL,
+    added_date DATETIME DEFAULT (CURRENT_DATE()),
+    PRIMARY KEY(rel_id)
+);
+
+DROP TABLE IF EXISTS release_notification;
+CREATE TABLE IF NOT EXISTS release_notification(
+    notification_id INT AUTO_INCREMENT,
+    id_user INT NOT NULL,
+    id_game INT NOT NULL,
+    PRIMARY KEY (notification_id)
+);
+
+insert into user(username, email, pwd) values("test", "test@gmail.com", "test");
+insert into user(username, email, pwd) values("test2", "test2@gmail.com", "test");
