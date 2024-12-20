0eb1c513f58f53c05affb6df6f35805daf4eccb6
GaelDPaquin <gaeldpaquin@gmail.com>
2024-12-20 16:35:12 -0500

    home screen styling

--- app/index.jsx
+++ app/index.jsx
@@ -5,6 +5,7 @@ import { TouchableOpacity } from 'react-native'
 import { useTheme } from '../contexts/ThemeContext'
 import { Link, useRouter} from 'expo-router'
 import { colorsPalette } from '../assets/colorsPalette'
+import { Header } from 'react-native/Libraries/NewAppScreen'
 
 const  WIDTH_BTN = Dimensions.get('window').width - 56
 
@@ -16,19 +17,26 @@ const index = () => {
 
     return (
 
-        <SafeAreaView  className="flex-1 justify-evenly items-center">
-            <ScrollView>
-                <Text className={"text-6xl font-bold tracking-[2px] text-center uppercase"}>This is the main page</Text>
+        <SafeAreaView  className="flex-1 justify-center items-center bg-zinc-200 align-middle">
+            <ScrollView className="">
+                <Text className={"text-6xl font-bold tracking-[2px] text-center uppercase mt-20 mx-6"}>Pick your games</Text>
+                <Text className="text-2xl mx-14 mt-10 capitalize font-light antialiased ">Access an extensible game catalogue, but most importantly, find what is your OBJECTIVE favorite Video Game to exist...</Text>
+                <SafeAreaView id='btns' className="items-center content-center">
+                    <Text className="my-6 text-xl text-center cap uppercase w-full font-bold">Start Picking now</Text>
+                    <TouchableOpacity className={`rounded bg-orange-400 border-solid border-orange-500 border-2 rounded-3xl w-40 h-24 justify-center`}  onPress={() => { router.push("./auth/signup")}}>
+                        <Text className={`text-lg text-center align-middle font-bold`} style={{color:colors.lightText}} >Create a new account</Text>
+                    </TouchableOpacity>
 
-                <TouchableOpacity className={`rounded p-6 my-4`} style={{backgroundColor:colors.primary}} onPress={() => { router.push("./auth/signup")}}>
-                <Text className={`text-2xl`} style={{color:colors.lightText}} >Sign-up</Text>
-                </TouchableOpacity>
-
-
-  
-                <TouchableOpacity className={`rounded p-6 my-4`} style={{backgroundColor:colors.primary}} onPress={() => { router.push("./auth/signin")}}>
-                <Text className={`text-2xl`} style={{color:colors.lightText}} >Sign-in</Text>
-                </TouchableOpacity>
+                    <SafeAreaView className="flex flex-col justify-center items-center mt-10 ">
+                        <Text className="my-6 text-center capitalize w-full">Already have an account?</Text>
+                        <TouchableOpacity className={`rounded bg-orange-400 border-solid border-orange-500 border-2 rounded-3xl w-32 h-16  justify-center`} onPress={() => { router.push("./auth/signin")}}>
+                            <Text className={`text-lg text-center align-middle`} style={{color:colors.lightText}} >Sign in</Text>
+                        </TouchableOpacity>
+                    </SafeAreaView>
+                    
+                </SafeAreaView>
+                
+                
            
             </ScrollView>
         </SafeAreaView>
