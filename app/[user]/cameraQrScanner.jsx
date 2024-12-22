928f1e6c1c5cbdf3c2607a77f5a0e2f33200698b
Jimmy <jimmy.n4444@gmail.com>
2024-12-21 23:57:49 -0500

    advancing the qr code system, not working

---
+++ app/[user]/cameraQrScanner.jsx
@@ -0,0 +1,131 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { CameraView, CameraType, useCameraPermissions,Camera } from 'expo-camera';
+import { Button, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {useRouter,useGlobalSearchParams} from "expo-router"
+import { useState } from "react";
+import { addFriend } from "../../lib/axios";
+
+const cameraQrScanner=()=>{
+    const [permission, requestPermission] = useCameraPermissions();
+    const [isScanned, setIsScanned] = useState(false);
+    const router=useRouter();
+    const glob = useGlobalSearchParams();
+    const [friendId, setFriendId] = useState(""); 
+    const [isLoading, setIsLoading] = useState(false);
+    
+    
+
+    const scanning=async(friendId)=>{
+        if(isScanned){
+            return;
+        }
+        setIsScanned(true)
+        setFriendId(friendId.friendId);     
+        try{
+            await handleAddFriend()
+            router.back();
+        }   catch(error){
+            console.log("Error",error)
+        }
+    }
+    const handleAddFriend=async()=>{
+        if (isLoading) return; 
+        setIsLoading(true);
+         const [isLoading, setIsLoading] = useState(false);
+        try{
+            const addingFriend=await addFriend(glob.user,friendId)
+            console.log("success in adding friend")
+        }catch(error){
+            console.log(error);
+        }
+        finally {
+            setIsLoading(false);
+        }
+    }
+    if (!permission) {
+        // Camera permissions are still loading.
+        return <View />;
+      }
+
+      if (!permission.granted) {
+        // Camera permissions are not granted yet.
+        return (
+          <View style={styles.container}>
+            <Text style={styles.message}>We need your permission to show the camera</Text>
+            <Button onPress={requestPermission} title="grant permission" />
+          </View>
+        );
+      }
+
+return (
+    <SafeAreaView style={styles.container}>
+        <Text>QR code scanner to add friend</Text>
+        <CameraView  style={styles.camera} 
+        facing="back"
+        type={CameraView.Type.back}
+        onBarcodeScanned={scanning}>
+            <View style={styles.overlay}>
+            <View style={styles.scannerWindow}></View>
+            </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={() => {router.back()}} >
+            <Text style={styles.text}>Exit</Text>
+          </TouchableOpacity>
+          </View>
+
+        </CameraView>
+       
+    </SafeAreaView>
+)
+
+
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+    },
+    message: {
+      textAlign: 'center',
+      paddingBottom: 10,
+    },
+    camera: {
+      flex: 1,
+    },
+    buttonContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      backgroundColor: 'transparent',
+      margin: 64,
+    },
+    button: {
+      flex: 1,
+      alignSelf: 'flex-end',
+      alignItems: 'center',
+    },
+    text: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      color: 'white',
+    },
+    overlay: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        
+    },
+    scannerWindow: {
+        width: '80%', 
+        height: '40%',
+        borderWidth: 2,
+        borderColor: '#fff',
+    },
+  });
+
+export default cameraQrScanner;
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -5,7 +5,6 @@ import { Drawer } from 'expo-router/drawer';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import CustomDrawerHeader from '../components/CustomDrawerHeader';
 import { ThemeProvider } from '../contexts/ThemeContext';
-
 import "../global.css"; 
 
 const RootLayout = () => {
@@ -38,6 +37,9 @@ const Layout = () => {
               <Drawer.Screen name="menu" options={{headerShown:false}} />
               <Drawer.Screen name="[user]/friendForm"  options={{ headerShown: true }}/>
               <Drawer.Screen name="[user]/showAllFriends"  options={{ headerShown: true }}/>
+              <Drawer.Screen name="[user]/cameraQrScanner"  options={{ headerShown: true }}/>
+              <Drawer.Screen name="[user]/qrCodeUser"  options={{ headerShown: true }}/>
+
           </Drawer>
         </GestureHandlerRootView>
 
