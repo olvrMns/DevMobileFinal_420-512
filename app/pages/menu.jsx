3072e46acf66622943300d284d7e839726ef01b2
GaelDPaquin <gaeldpaquin@gmail.com>
2024-12-22 17:49:31 -0500

    menu fini

--- app/pages/menu.jsx
+++ app/pages/menu.jsx
@@ -4,24 +4,35 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { Text, View} from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import { ScrollView } from "react-native-gesture-handler";
+import { router } from "expo-router";
 
 const Menu = () => {
+
+    const handleCataloguePress = () => {
+        router.push("../../game/catalog");
+      }
+    const handleFavPress = () => {
+    router.push("../../game/favortePicker");
+    }
+    const handleFriendsPress = () => {
+        router.push("../../user/showAllFriends");
+    }
     return(
         <>
-        <View className="flex-col bg-orange-100 h-full w-full justify-center ">
+        <View className="flex-col bg-orange-100 h-full w-full justify-center md:space-x-1">
             <ScrollView>
             <SafeAreaView id="menuContainer">
-                        <TouchableOpacity className="bg-orange-400 border-2 rounded-2xl w-5/6 px-32 py-24 justify-start m-10 flex-row">
-                            <Text className="text-white text-3xl w-full justify-start">Catalogue</Text>
-                            <Icon name="book" className="w-1/5"></Icon>
+                        <TouchableOpacity onPress={handleCataloguePress} className="bg-orange-300 border-2 rounded-3xl w-5/6 px-20 py-20 justify-start mx-10 my-2 flex-row items-center">
+                            <Text className="text-white text-3xl w-full justify-start text-nowrap">Catalogue</Text>
+                            <Icon name="book" className="w-1/5" size={30}></Icon>
                         </TouchableOpacity>
-                        <TouchableOpacity className="bg-orange-400 border-2 rounded-2xl w-5/6 px-32 py-24 justify-start m-10 flex-row">
-                            <Text className="text-white text-3xl w-full justify-start">Favorite Picker</Text>
-                            <Icon name="heart" className="w-1/5"></Icon>
+                        <TouchableOpacity onPress={handleFavPress} className="bg-orange-400 border-2 rounded-3xl w-5/6 px-20 py-20 justify-start mx-10 my-2 flex-row items-center">
+                            <Text className="text-white text-3xl w-full justify-start text-nowrap">Favorite Picker</Text>
+                            <Icon name="heart" className="w-1/5" size={30}></Icon>
                         </TouchableOpacity>
-                        <TouchableOpacity className="bg-orange-400 border-2 rounded-2xl w-5/6 px-32 py-24 justify-start m-10 flex-row">
-                            <Text className="text-white text-3xl w-full justify-start">Friends</Text>
-                            <Icon name="user" className="w-1/5"></Icon>
+                        <TouchableOpacity onPress={handleFriendsPress} className="bg-orange-500 border-2 rounded-3xl w-5/6 px-20 py-20 justify-start mx-10 my-2 flex-row items-center">
+                            <Text className="text-white text-3xl w-full justify-start text-nowrap">Friends</Text>
+                            <Icon name="user" className="w-1/5 mr-" size={30}></Icon>
                         </TouchableOpacity>
                 </SafeAreaView>
             </ScrollView>
