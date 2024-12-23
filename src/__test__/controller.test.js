412274da190962184aa89c419055775292edc53c
olvrMns <oliviermansuy7@gmail.com>
2024-12-23 15:28:33 -0500

    tests...

--- src/__test__/controller.test.js
+++ src/__test__/controller.test.js
@@ -1,16 +1,18 @@
 import supertest from "supertest";
 import { app } from "../app";
 
+const regularTest = async (endpoint, body, expectedCode) => {
+    const res = await supertest(app).post(endpoint).send(body);
+    expect(res.statusCode).toBe(expectedCode);
+}
+
 describe("Acceptance testing", () => {
 
     describe("Auth", () => {
-        it("login", async () => {
-            const res = (await supertest(app).post("/users/signin")).setEncoding({usernameOrEmail: "test", pwd: "test"});
-        })
+        it("sigin", async () => regularTest("/users/signin", {usernameOrEmail: "test", pwd: "test"}, 200))
 
-        // it("signUp", async () => {
-            
-        // })
+        //async () => regularTest("/users", {username: "salut", pwd: "salut", email: "salut@salut.com"}, 201)
+        it("signUp", async () => regularTest("/users", {username: "salut2", pwd: "salut2", email: "salut2@salut.com"}, 201))
     })
 
     describe("game", () => {
