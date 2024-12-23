369e1e10f0545801e87ff87cf739b413a3e8d008
GaelDPaquin <gaeldpaquin@gmail.com>
2024-12-23 17:06:56 -0500

    getGamesTest et coverage

--- tests/axios.test.js
+++ tests/axios.test.js
@@ -1,8 +1,10 @@
-import { getGames, setToken, getToken } from "../../lib/axios";
+import { getGames, setToken, getToken, signUp, signIn, fetchProfileData, getIdFromJwt } from "../lib/axios";
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 jest.mock('@react-native-async-storage/async-storage');
+
+
 
 describe('setToken', () => {
     it('test if the token is set (for auth user)', async () => {
@@ -22,3 +24,21 @@ describe('getToken', () => {
         expect(res).toBe('tokenfortesting');
     })
 })
+
+describe('getGames', () => {
+    it('test if can get all games from api', async () => {
+        let res = await getGames(2,2);
+        let res2 = await getGames();
+        console.log(res);
+        expect(res).toHaveLength(2);
+        res.forEach(game => {
+            expect(game).toHaveProperty("id");
+            expect(game).toHaveProperty("name");
+            expect(game).toHaveProperty("released");
+            expect(game).toHaveProperty("ratings");
+            expect(game).toHaveProperty("metacritic");
+        });
+        expect(res2).toBeNull();
+    })
+})
+
