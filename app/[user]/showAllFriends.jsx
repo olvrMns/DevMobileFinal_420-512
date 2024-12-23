08add5872a2c767a3e20406d2ca579b5655447b8
GaelDPaquin <gaeldpaquin@gmail.com>
2024-12-22 23:51:44 -0500

    fixes, visual of qrcodes, etc. Tests tomorrow<

--- app/[user]/showAllFriends.jsx
+++ app/[user]/showAllFriends.jsx
@@ -1,14 +1,16 @@
 import { Image, Text, View, TextInput, ScrollView,TouchableOpacity, Modal, FlatList, Dimensions,ActivityIndicator} from 'react-native'
 import React, { useState, useEffect } from 'react';
 import { getAllFriendsByUserId } from '../../lib/axios'
-import {useGlobalSearchParams } from 'expo-router';
+import {useRouter, useGlobalSearchParams } from 'expo-router';
 import { useTheme } from '../../contexts/ThemeContext'
 import { colorsPalette } from '../../assets/colorsPalette'
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const showAllFriends=()=>{
     const [friends, setFriends] = useState([]);
     const glob = useGlobalSearchParams();
     const {theme} = useTheme()
+    const router = useRouter();
     const colors = colorsPalette[theme]
     const [loading, setLoading] = useState(false);
     const [isMounted, setIsMounted] = useState(false);
@@ -51,23 +53,43 @@ const showAllFriends=()=>{
     )
   }
 
-  return (
-    <View>
-        {loading ? (
-      <ActivityIndicator size="large" color={colors.text} />
-    ) : (
-        <>
-        <Text>Friends list</Text>
-        <FlatList
-        data={friends}
-        keyExtractor={(item) => item.user_id.toString()}
-        renderItem={renderFriends}
-        
-        />
-        </>
-         )}
-    </View>
+  const handleCamera = () =>{
+    router.push(glob.user + "/" + "cameraQrScanner");
+  }
+  const handleForm = () =>{
+    router.push(glob.user + "/" + "friendForm");
+  }
 
+  return (
+    <>
+      <View>
+          
+        <View className="">
+          <View className="w-16 flex-row justify-between">
+            <TouchableOpacity className="items- mx-2" onPress={handleCamera}>
+              <Icon name="camera" size={30}></Icon>
+            </TouchableOpacity>
+            <TouchableOpacity className="items-center mx-2" onPress={handleForm}>
+              <Icon name="plus" size={30}></Icon>
+            </TouchableOpacity>
+          </View>
+          
+            {loading ? (
+          <ActivityIndicator size="large" color={colors.text} />
+        ) : (
+            <>
+            <Text className="mt-10 text-center text-orange-500 font-bold text-4xl ">Friends list</Text>
+            <FlatList className="mt-10"
+            data={friends}
+            keyExtractor={(item) => item.user_id.toString()}
+            renderItem={renderFriends}
+            
+            />
+            </>
+            )}
+        </View>
+      </View>
+    </>
 
   )
 
